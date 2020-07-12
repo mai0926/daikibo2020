810d206c25a3766a4d230e6809679fe93e67ce9a
kazuki19992 <kushida98@gmail.com>
2020-07-12 16:05:34 +0900

    aaa

--- WEB-INF/src/TimetableServlet.java
+++ WEB-INF/src/TimetableServlet.java
@@ -16,54 +16,53 @@ public class TimetableServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) 
         throws IOException, ServletException {
 		String mode = request.getParameter("mode");
-		
+
 		if(mode.equals("write")){
 		    String youbi = request.getParameter("day"); // 1から 6
 		    String period = request.getParameter("period"); // 1から6
 		    String subject_name = request.getParameter("subject"); // subject_name
 		    int day = -1; // day <- youbi(int)
 		    int period_int = -1; // period_int <- period(int)
-		    
+
 		    try{ // 受け取った曜日とn時限のデータ(String)をintに変換
-			day = Integer.parseInt(youbi);
-			period_int = Integer.parseInt(period);
+				day = Integer.parseInt(youbi);
+				period_int = Integer.parseInt(period);
 		    }catch (NumberFormatException e){
-			
+
 		    }
-		    
+
 		    Timetable.putSubject(day, period_int, subject_name);
 
  		    String Subject[][] = new String[6][6];
 		    int i,j;
 		    for( i = 0; i < 6; i++ ){
-			for( j = 0; j < 6; j++ ){
-			    Subject[i][j] = Timetable.getSubject( i, j );
-			}
+				for( j = 0; j < 6; j++ ){
+					Subject[i][j] = Timetable.getSubject( i, j );
+				}
 		    }
 
 		    request.setAttribute("subject", Subject);
 		    RequestDispatcher disp = request.getRequestDispatcher("/jsp/timetable.jsp");
 		    disp.forward(request, response);
-		    
-		    
+
 		}else if(mode.equals("read")){
 		    String youbi = request.getParameter("day");
 		    String period = request.getParameter("period");
 		    String subject = request.getParameter("subject");
-		    
+
 		    String Subject[][] = new String[6][6];
 		    int i,j;
 		    for( i = 0; i < 6; i++ ){
-			for( j = 0; j < 6; j++ ){
-			    Subject[i][j] = Timetable.getSubject( i, j );
-			}
+				for( j = 0; j < 6; j++ ){
+					Subject[i][j] = Timetable.getSubject( i, j );
+				}
 		    }
 
 		    request.setAttribute("subject", Subject);
 		    RequestDispatcher disp = request.getRequestDispatcher("/jsp/timetable.jsp");
 		    disp.forward(request, response);
-		    
+
 		}
-		
+
     }
 }
